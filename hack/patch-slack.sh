94fbe31efe7619ff96d0d829f52706301672b70a
Paulo Gomes <paulo.gomes@suse.com>
2023-12-10 23:07:45 +0000

    build: Auto-patch Slack
    Signed-off-by: Paulo Gomes <paulo.gomes@suse.com>

---
+++ hack/patch-slack.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SLACK_URL="https://slack.com/intl/en-gb/release-notes/linux"
+SLACK_REGEX="Slack ([0-9].[0-9]{1,2}.[0-9]{1,3})"
+
+function main() {
+    local LATEST_VERSION=""
+    LATEST_VERSION=$(curl "${SLACK_URL}" | grep -oEi "${SLACK_REGEX}" | head -n1 | cut -d' ' -f2)
+
+    if [ -n "${LATEST_VERSION}" ]; then
+        sed -i "s;ARG SLACK_VERSION=.*;ARG SLACK_VERSION=${LATEST_VERSION};g" workloads/slack/Dockerfile
+    fi
+
+    if git diff --cached --quiet; then
+        echo "Update to ${LATEST_VERSION}"
+        
+        git checkout -b slack-patch
+        git add workloads/slack/Dockerfile
+        git commit -m "build: Bump Slack to ${LATEST_VERSION}"
+        gh pr create --fill --head main --base slack-patch
+    else
+        echo "No new versions found."
+    fi
+}
+
+main
