807aef7ed3613a97e68ff09b67682c4db54c9c08
Paulo Gomes <paulo.gomes@suse.com>
2023-12-13 20:57:45 +0000

    build: Change base branch for pr creation
    Signed-off-by: Paulo Gomes <paulo.gomes@suse.com>

--- hack/patch-slack.sh
+++ hack/patch-slack.sh
@@ -19,7 +19,7 @@ function main() {
         git checkout -b slack-patch
         git add workloads/slack/Dockerfile
         git commit -m "build: Bump Slack to ${LATEST_VERSION}"
-        gh pr create --title "build: Bump Slack to ${LATEST_VERSION}" --body "" --head main --base slack-patch
+        gh pr create --title "build: Bump Slack to ${LATEST_VERSION}" --body "" --base main
     else
         echo "No new versions found."
     fi
